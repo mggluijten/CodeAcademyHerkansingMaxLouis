bc5e065e32837af36a14a7f2592d3e1133649b6b
LouisBaalt <lm.vanbaalen@student.avans.nl>
2024-03-29 18:11:23 +0100

    piechart added

--- ApplicationLogicLayer/TopDrieOverzicht.java
+++ ApplicationLogicLayer/TopDrieOverzicht.java
@@ -6,8 +6,12 @@ import java.sql.SQLException;
 
 import Data_Access_Layer_DAL.DatabaseConnection;
 import ApplicationLogicLayer.CodeAcademyMaxLouisAPP;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Side;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
@@ -54,6 +58,9 @@ public class TopDrieOverzicht {
     @FXML
     private Label webcastLabelTwee;
 
+    @FXML
+    private PieChart pieChartWebcasts;
+
     CodeAcademyMaxLouisAPP m = new CodeAcademyMaxLouisAPP();
     DatabaseConnection connection = new DatabaseConnection();
 
@@ -73,7 +80,7 @@ public class TopDrieOverzicht {
     @FXML
     void initialize() {
         labelsVanWebcast();
-
+        piechartVanWebcast();
     }
 
     private void labelsVanCursussen() {
@@ -149,6 +156,41 @@ public class TopDrieOverzicht {
                 webcastLabelDrie.setText(cursusNamenWebcast[2]);
                 cursistLabelDrie.setText(aantalBekeken[2] + " keer bekeken");
 
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                connection.closeConnection();
+            }
+        }
+    }
+
+    private void piechartVanWebcast() {
+        if (connection.openConnection()) {
+            try {
+                String queryWebcast = "SELECT TOP (3) Inschrijving.CursusNaam, Count(*) as meest_bekeken " +
+                        "FROM Inschrijving " +
+                        "JOIN ContentItem ON ContentItem.CursusNaam = Inschrijving.CursusNaam " +
+                        "WHERE ContentItem.ModuleID IS NULL " +
+                        "GROUP BY Inschrijving.CursusNaam " +
+                        "ORDER BY meest_bekeken DESC";
+
+                ResultSet resultSet = connection.executeSQLSelectStatement(queryWebcast);
+
+                // Maak arrays om de gegevens op te slaan
+                String[] cursusNamenWebcast = new String[3];
+                int[] aantalBekeken = new int[3];
+
+                int index = 0;
+                while (resultSet.next() && index < 3) {
+                    cursusNamenWebcast[index] = resultSet.getString("CursusNaam");
+                    aantalBekeken[index] = resultSet.getInt("meest_bekeken");
+                    index++;
+                }
+                ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList(
+                        new PieChart.Data(cursusNamenWebcast[0], aantalBekeken[0]),
+                        new PieChart.Data(cursusNamenWebcast[1], aantalBekeken[1]),
+                        new PieChart.Data(cursusNamenWebcast[2], aantalBekeken[2]));
+                pieChartWebcasts.setData(pieChartData);
             } catch (SQLException e) {
                 e.printStackTrace();
             } finally {
